2ab2b639b305ecd81bff9e3ced69d9be0f3295b5
Feth Arezki <feth@majerti.fr>
2015-06-30 22:21:25 +0200

    Send mail when complete

--- run_backup.py
+++ run_backup.py
@@ -27,8 +27,20 @@ import configparser
 import datetime
 import logging
 import os
+import smtplib
 import subprocess
 import sys
+
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+
+
+class BackupException(Exception):
+    pass
+
+
+class ConfigException(BackupException):
+    pass
 
 
 StreamHandler = getattr(logging, 'StreamHandler', None)
@@ -47,9 +59,20 @@ Context = collections.namedtuple(
         'src_dir',
         'dst_dir',
         'logfile',
+        'mail',
         'log_out_fd',
         'log_err_fd',
         'log_ret_fd',
+    )
+)
+
+
+MailConfig = collections.namedtuple(
+    'MailConfig',
+    (
+        'mailto',
+        'mailfrom',
+        'smtp',
     )
 )
 
@@ -91,10 +114,13 @@ def backup(context, logger):
     context.log_ret_fd.write('{0}\n'.format(process.returncode))
     logfn('{0[0]} exited with status {1}'.format(command, returncode))
 
+    for descr in (context.log_out_fd, context.log_err_fd, context.log_ret_fd):
+        descr.close()
+
 
 def _config_file():
     """
-    Locate the config file or raises Exception
+    Locate the config file or raises ConfigException
     """
     config_fname = 'run_backup.rc'
     candidates = []
@@ -111,7 +137,7 @@ def _config_file():
     if os.path.exists(candidate):
         return candidate
     candidates.append(candidate)
-    raise Exception("No config file. Places examined: {}".format(
+    raise ConfigException("No config file. Places examined: {}".format(
         ', '.join(candidates)
     ))
 
@@ -124,13 +150,38 @@ def context(section_name='main_backup'):
     config_fname = _config_file()
     config.read(config_fname)
     if section_name not in config:
-        raise Exception("Need a '{}' section in {}".format(
+        raise ConfigException("Need a '{}' section in {}".format(
             section_name, config_fname
         ))
     host = config[section_name]['host']
     src_dir = config[section_name]['src_dir']
     dst_dir = config[section_name]['dst_dir']
     logbase = config[section_name]['logbase']
+
+    mailto = config[section_name].get('mailto', None)
+    if mailto:
+        if not config.get('mail', None):
+            raise ConfigException(
+                "mailto not null in section {0}, but no section 'mail'".format(
+                    section_name
+                )
+            )
+        mailfrom = config[section_name].get('mailfrom', None)
+        if not mailfrom:
+            mailfrom = config['mail'].get('mailfrom', None)
+        if not mailfrom:
+            raise ConfigException(
+                "Please specify mailfrom either in '{0}' or in 'mail'".format(
+                    section_name
+                )
+            )
+        mailcfg = MailConfig(
+            mailto,
+            mailfrom,
+            config['mail'].get('smtp', 'smtp')
+        )
+    else:
+        mailcfg = None
 
     # if path does not end with /, trouble ahead with rsync.
     src_dir, dst_dir = (
@@ -164,7 +215,8 @@ def context(section_name='main_backup'):
             logbase, nowdir, '{}.log'.format(
                 os.path.basename(sys.argv[0])
             )
-        )
+        ),
+        mailcfg,
         ) + tuple(log_fds)
     print(ctx_args)
     return Context(*ctx_args)
@@ -179,6 +231,52 @@ def setup_log(context):
     logger.addHandler(StreamHandler(sys.stdout))
     return logger
 
+
+def log2mail(context):
+
+    if os.path.exists(context.log_ret_fd.name):
+        returncode = open(context.log_ret_fd.name).read().strip()
+    else:
+        returncode = "unknown"
+
+    taskdesc = 'backup of {0.host}:{0.src_dir} on {0.dst_dir}'.format(context)
+
+    if returncode == '0':
+        subject = '[success] {}'.format(taskdesc)
+    else:
+        subject = '[failed] {}'.format(taskdesc)
+    msg = MIMEMultiPart(
+        Subject=subject,
+        From=context.mail.mailfrom,
+        To=context.mail.mailto,
+    )
+
+    msg.preamble = '{}\nreturncode of rsync was {}\n'.format(
+        subject,
+        returncode
+    )
+
+    for name, fname in (
+            'stdout.txt', context.log_out_fd.name,
+            'stderr.txt', context.log_err_fd.name,
+            ):
+        textfile = MIMEApplication(
+            open(fname).read(),
+            Content_Disposition='attachment; filename="{}"'.format(name)
+        )
+        msg.attach(
+            textfile
+        )
+
+    session = smtplib.SMTP(context.mail.smtp)
+    session.sendmail(
+        context.mail.mailfrom,
+        [context.mail.mailto],
+        msg.as_string()
+    )
+    session.quit()
+
+
 if __name__ == '__main__':
     context = context()
 
@@ -188,3 +286,6 @@ if __name__ == '__main__':
         backup(context, logger)
     except BaseException as err:
         logger.exception("An error or interruption occured")
+    finally:
+        if context.mail:
+            log2mail(context)
